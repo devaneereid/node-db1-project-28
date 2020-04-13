990e0b47cfc0ddcf0e16fba65dcaa4f6ccba7725
Devanee Reid <devaneereid@gmail.com>
2020-04-13 16:51:29 -0600

    set up postgres - final commit

--- knexfile.js
+++ knexfile.js
@@ -17,12 +17,13 @@ module.exports = {
   // update the following configuration to use PostgreSQL
   production: {
     client: "pg",
-    connection: {
-      host: "localhost", // if the server is not running on your computer provide the network address
-      database: "the name of the database to use in the postgres server", // <-- update
-      user: "a user that has access to the server and database", // <-- update
-      password: "the password for the user", // <-- update
-    },
+    connection: 'postgresql://localhost', 
+      
+      // host: "localhost", // if the server is not running on your computer provide the network address
+      database: "accounts_db", // <-- update
+      user: "username", // <-- update
+      password: "password", // <-- update
+    
     pool: {
       min: 2,
       max: 10,
