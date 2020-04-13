4d0d0e3952ced90a1a9368129ca6617abbb251e2
Devanee Reid <devaneereid@gmail.com>
2020-04-13 14:07:26 -0600

    created post router and CRUD requests

--- api/server.js
+++ api/server.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const db = require("../data/dbConfig.js");
 
+const PostRouter = require('../posts/post-router.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', PostRouter);
+
+server.get('/', (req, res) => {
+    res.send({ message: 'API is up and running' });
+});
 
 module.exports = server;
---
+++ posts/post-router.js
@@ -0,0 +1,85 @@
+const express = require('express');
+
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+// GET Request
+router.get('/', (req, res) => {
+    db.select('*')
+        .from('accounts')
+        .then(accounts => {
+            res.status(200).json({ data: accounts });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: 'GET request not reached' });
+        });
+});
+
+// GET (ID's) Request
+router.get('/:id', (req,res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .first()
+        .then(accounts => {
+            res.status(200).json({ data: accounts });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: 'Error retrieving Account ID'});
+        });
+});
+
+// POST Request
+router.post('/', (req, res) => {
+    const postData = req.body;
+        db('accounts')
+            .insert(postData, 'id')
+            .then(ids => {
+                const id = ids[0];
+                db('accounts')
+                .where({ id })
+                .first()
+                .then(post => {
+                    res.status(201).json({ data: post });
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ error: 'Error creating new post' });
+            });
+});
+
+// PATCH Request 
+router.patch('/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+    db('accounts')
+        .where({ id })
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: 'Update request successful' })
+            } else {
+                res.status(404).json({ message: 'No Accounts by that ID found' });
+            }
+        });
+});
+
+// DELETE Request
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    db('accounts')
+        .where({ id })
+        .del()
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: 'Failed to Delete account' })
+        });
+});
+
+module.exports = router;
